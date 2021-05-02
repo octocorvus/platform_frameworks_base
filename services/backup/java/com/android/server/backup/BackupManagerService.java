c3817ed6614304eff1ad667ec251b80dce9a1f28
Pratyush <39559233+empratyush@users.noreply.github.com>
2021-05-02 00:36:30 +0000

    enable backup service for non-owner users

--- services/backup/java/com/android/server/backup/BackupManagerService.java
+++ services/backup/java/com/android/server/backup/BackupManagerService.java
@@ -55,6 +55,8 @@ import android.os.SystemProperties;
 import android.os.Trace;
 import android.os.UserHandle;
 import android.os.UserManager;
+import android.provider.Settings;
+import android.util.Log;
 import android.util.Slog;
 import android.util.SparseArray;
 
@@ -174,6 +176,19 @@ public class BackupManagerService extends IBackupManager.Stub implements BackupM
      */
     private boolean mHasFirstUserUnlockedSinceBoot = false;
 
+    private final BroadcastReceiver mUserAddedReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (Intent.ACTION_USER_ADDED.equals(intent.getAction())) {
+                int userId = intent.getIntExtra(Intent.EXTRA_USER_HANDLE, UserHandle.USER_NULL);
+                Log.d(TAG, "new user added User ID : " + userId);
+                if (userId > 0) {
+                    mHandler.post(() -> setBackupServiceActive(userId, true));
+                }
+            }
+        }
+    };
+
     public BackupManagerService(Context context) {
         mContext = context;
         mGlobalDisable = isBackupDisabled();
@@ -193,6 +208,36 @@ public class BackupManagerService extends IBackupManager.Stub implements BackupM
             // This might happen on the first boot if BMS starts before the main user is created.
             Slog.d(TAG, "Main user does not exist yet");
         }
+
+        mContext.registerReceiver(
+                mUserAddedReceiver, new IntentFilter(Intent.ACTION_USER_ADDED));
+
+        if (shouldMigrateExistingUser(context)) {
+            List<UserInfo> managerUsers = mUserManagerInternal.getUsers(true);
+                for (UserInfo userInfo : managerUsers) {
+                    setBackupServiceActive(
+                        userInfo.getUserHandle().getIdentifier(),
+                        true
+                    );
+            }
+
+            markExistingUserMigrated(context);
+        }
+
+    }
+
+    private static final String PROFILE_MIGRATION_COMPLETED = "graphene_existing_users_migration";
+
+    private boolean markExistingUserMigrated(Context context) {
+        return Settings.Global.putInt(context.getContentResolver(), PROFILE_MIGRATION_COMPLETED, 1);
+    }
+
+    private boolean shouldMigrateExistingUser(Context context) {
+        return Settings.Global.getInt(
+                context.getContentResolver(),
+                PROFILE_MIGRATION_COMPLETED,
+                0
+            ) != 1;
     }
 
     @VisibleForTesting
