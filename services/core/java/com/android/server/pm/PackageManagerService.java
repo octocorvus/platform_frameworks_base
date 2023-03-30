6e755b4992ef34feb73c87c287b9b841d5da08e7
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-03-30 10:35:08 +0300

    infrastructure for creating per-package seinfo overrides

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -222,6 +222,7 @@ import com.android.server.compat.CompatChange;
 import com.android.server.compat.PlatformCompat;
 import com.android.server.crashrecovery.CrashRecoveryAdaptor;
 import com.android.server.ext.PackageManagerHooks;
+import com.android.server.ext.SeInfoOverride;
 import com.android.server.pm.Installer.InstallerException;
 import com.android.server.pm.Settings.VersionInfo;
 import com.android.server.pm.dex.ArtManagerService;
@@ -1788,6 +1789,8 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
                 selinuxChangeListener);
         injector.getCompatibility().registerListener(SELinuxMMAC.SELINUX_R_CHANGES,
                 selinuxChangeListener);
+
+        m.selinuxChangeListener = selinuxChangeListener;
 
         m.installAllowlistedSystemPackages();
         IPackageManagerImpl iPackageManager = m.new IPackageManagerImpl();
@@ -6871,6 +6874,11 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
         }
 
         @Override
+        public void updateSeInfo(String packageName) {
+            SeInfoOverride.updateSeInfo(PackageManagerService.this, packageName);
+        }
+
+        @Override
         public void sendBootCompletedBroadcastToPackage(String packageName, boolean includeStopped,
                                                     int userId) {
             mContext.enforceCallingPermission(Manifest.permission.GRANT_RUNTIME_PERMISSIONS, null);
@@ -8467,6 +8475,13 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
     @Nullable
     public String getSystemAppUpdateOwnerPackageName(String packageName) {
         return mInjector.getSystemConfig().getSystemAppUpdateOwnerPackageName(packageName);
+    }
+
+    private CompatChange.ChangeListener selinuxChangeListener;
+
+    public void updateSeInfo(String packageName) {
+        // use the same procedure that is used for SELinux compat changes
+        selinuxChangeListener.onCompatChange(packageName);
     }
 
     @NonNull
