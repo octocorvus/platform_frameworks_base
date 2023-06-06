97d5d5a8ca5e4304baabe526da3e54d9521b607b
GrapheneOS <contact@grapheneos.org>
2023-06-06 14:44:05 +0300

    gmscompat: add compatibility layer for unprivileged GMS
    Originally authored by Danny Lin <danny@kdrag0n.dev> for inclusion in
    GrapheneOS. It has since been substantially extended and partially
    rewritten by Dmitry Muhomor <muhomor.dmitry@gmail.com> (primarily) and
    Daniel Micay <danielmicay@gmail.com>.
    
    Commit history:
    (better viewed with git log --grep gmscompat $TAG_NAME)
    
    https://github.com/GrapheneOS/platform_frameworks_base/commits/2025081400
    https://github.com/GrapheneOS/platform_frameworks_base/commits/2025060200
    https://github.com/GrapheneOS/platform_frameworks_base/commits/2025021100
    https://github.com/GrapheneOS/platform_frameworks_base/commits/2024112700
    https://github.com/GrapheneOS/platform_frameworks_base/commits/2024083100
    https://github.com/GrapheneOS/platform_frameworks_base/commits/2024053100
    https://github.com/GrapheneOS/platform_frameworks_base/commits/2024030300
    https://github.com/GrapheneOS/platform_frameworks_base/commits/2023112900
    https://github.com/GrapheneOS/platform_frameworks_base/commits/2023080800
    https://github.com/GrapheneOS/platform_frameworks_base/commits/TQ2A.230505.002.2023052900
    https://github.com/GrapheneOS/platform_frameworks_base/commits/TQ1A.230205.002.2023020600
    https://github.com/GrapheneOS/platform_frameworks_base/commits/SP2A.220505.006.2022073000
    https://github.com/GrapheneOS/platform_frameworks_base/commits/SP2A.220505.002.2022053100
    July 2021 - October 2021:
    https://github.com/GrapheneOS/platform_frameworks_base/commits/RQ3A.211001.001.2021100606

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -46,6 +46,7 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.SearchManager;
 import android.app.WallpaperManager;
+import android.app.compat.gms.GmsCompat;
 import android.compat.annotation.UnsupportedAppUsage;
 import android.content.AttributionSource;
 import android.content.ComponentName;
@@ -107,6 +108,7 @@ import android.view.WindowManager.LayoutParams;
 import android.widget.Editor;
 
 import com.android.internal.annotations.GuardedBy;
+import com.android.internal.gmscompat.GmsCompatApp;
 import com.android.internal.util.Preconditions;
 
 import java.io.IOException;
@@ -3639,9 +3641,31 @@ public final class Settings {
                     null);
         }
 
+        // Returns last path component of the relevant Uri.
+        // Keep in sync with GmsCompatApp#registerObserver
+        private String maybeGetGmsCompatNamespace() {
+            Uri uri = mUri;
+            // no need to use expensive equals() method in this case
+            if (uri == Global.CONTENT_URI) {
+                return "global";
+            }
+            if (uri == Secure.CONTENT_URI) {
+                return "secure";
+            }
+            return null;
+        }
+
         public boolean putStringForUser(ContentResolver cr, String name, String value,
                 String tag, boolean makeDefault, final @CanBeCURRENT @UserIdInt int userId,
                 boolean overrideableByRestore) {
+            if (GmsCompat.isEnabled()) {
+                String ns = maybeGetGmsCompatNamespace();
+                if (ns != null && !mAllFields.contains(name)) {
+                    return GmsCompatApp.putString(ns, name, value);
+                }
+                return false;
+            }
+
             try {
                 Bundle arg = new Bundle();
                 arg.putString(Settings.NameValueTable.VALUE, value);
@@ -3715,6 +3739,15 @@ public final class Settings {
         @UnsupportedAppUsage
         public String getStringForUser(ContentResolver cr, String name,
                 final @CanBeCURRENT @UserIdInt int userId) {
+            if (GmsCompat.isEnabled()) {
+                String ns = maybeGetGmsCompatNamespace();
+                if (ns != null) {
+                    if (!mAllFields.contains(name) && !name.startsWith("gmscompat")) {
+                        return GmsCompatApp.getString(ns, name);
+                    }
+                }
+            }
+
             final boolean isSelf = (userId == UserHandle.myUserId());
             final AttributionSource attributionSource = cr.getAttributionSource();
             final int deviceId =
@@ -3768,6 +3801,10 @@ public final class Settings {
             // still be regarded as readable.
             if (!isCallerExemptFromReadableRestriction() && mAllFields.contains(name)) {
                 if (!mReadableFields.contains(name)) {
+                    if (GmsCompat.isEnabled()) {
+                        return null;
+                    }
+
                     throw new SecurityException(
                             "Settings key: <" + name + "> is not readable. From S+, settings keys "
                                     + "annotated with @hide are restricted to system_server and "
@@ -3784,6 +3821,10 @@ public final class Settings {
                                 && application.getApplicationInfo().targetSdkVersion
                                 <= maxTargetSdk;
                         if (!targetSdkCheckOk) {
+                            if (GmsCompat.isEnabled()) {
+                                return null;
+                            }
+
                             throw new SecurityException(
                                     "Settings key: <" + name + "> is only readable to apps with "
                                             + "targetSdkVersion lower than or equal to: "
@@ -7320,6 +7361,11 @@ public final class Settings {
                 CALL_METHOD_DELETE_SECURE,
                 sProviderHolder,
                 Secure.class);
+
+        /** @hide */
+        public static boolean isKnownKey(String key) {
+            return sNameValueCache.mAllFields.contains(key);
+        }
 
         @UnsupportedAppUsage
         private static final HashSet<String> MOVED_TO_LOCK_SETTINGS;
@@ -19244,6 +19290,11 @@ public final class Settings {
                     sProviderHolder,
                     Global.class);
 
+        /** @hide */
+        public static boolean isKnownKey(String key) {
+            return sNameValueCache.mAllFields.contains(key);
+        }
+
         // Certain settings have been moved from global to the per-user secure namespace
         @UnsupportedAppUsage
         private static final HashSet<String> MOVED_TO_SECURE;
@@ -19526,6 +19577,19 @@ public final class Settings {
          * or not a valid integer.
          */
         public static int getInt(ContentResolver cr, String name, int def) {
+            if (GmsCompat.isEnabled()) {
+                if ("google_play_store_system_component_update".equals(name)) {
+                    // Stop Play Store from attempting to auto-install some system component
+                    // packages, such as "Android System SafetyCore" (com.google.android.safetycore)
+                    // and "Android System Key Verifier" (com.google.android.contactkeys)
+                    //
+                    // This setting also disables auto-updates of GmsCore and Play Store.
+                    if (!"1".equals(getString(cr, "gmscompat_bypass_sys_component_update_stub"))) {
+                        return 0;
+                    }
+                }
+            }
+
             String v = getString(cr, name);
             return parseIntSettingWithDefault(v, def);
         }
