4c11e08ca83d2a4b27561f0d9063456270ed6d2b
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-11-08 10:49:34 +0200

    fix rate limiting for non-app tombstones
    All non-app tombstones were considered to have the same processName ("UNKNOWN"), which meant that
    frequent tombstones from one system process rate-limited tombstones from all system processes.

--- services/core/java/com/android/server/os/NativeTombstoneManager.java
+++ services/core/java/com/android/server/os/NativeTombstoneManager.java
@@ -147,9 +147,9 @@ public final class NativeTombstoneManager {
 
         File protoPath = isProtoFile ? path : new File(path.getAbsolutePath() + ".pb");
 
-        final String processName = handleProtoTombstone(protoPath, isProtoFile)
-                .map(TombstoneFile::getProcessName)
-                .orElse("UNKNOWN");
+        String[] outProcessName = new String[1];
+        handleProtoTombstone(protoPath, isProtoFile, outProcessName);
+        final String processName = outProcessName[0] != null ? outProcessName[0] : "UNKNOWN";
 
         if (Flags.protoTombstone()) {
             BootReceiver.filterAndAddTombstoneToDropBox(mContext, path, processName, mTmpFileLock);
@@ -166,7 +166,7 @@ public final class NativeTombstoneManager {
         Reference.reachabilityFence(mWatcher);
     }
 
-    private Optional<TombstoneFile> handleProtoTombstone(File path, boolean addToList) {
+    private Optional<TombstoneFile> handleProtoTombstone(File path, boolean addToList, String[] outProcessName) {
         final String filename = path.getName();
         if (!filename.endsWith(".pb")) {
             Slog.w(TAG, "unexpected tombstone name: " + path);
@@ -196,7 +196,7 @@ public final class NativeTombstoneManager {
             return Optional.empty();
         }
 
-        final Optional<TombstoneFile> parsedTombstone = TombstoneFile.parse(pfd);
+        final Optional<TombstoneFile> parsedTombstone = TombstoneFile.parse(pfd, outProcessName);
         if (!parsedTombstone.isPresent()) {
             IoUtils.closeQuietly(pfd);
             return Optional.empty();
@@ -439,7 +439,7 @@ public final class NativeTombstoneManager {
             }
         }
 
-        static Optional<TombstoneFile> parse(ParcelFileDescriptor pfd) {
+        static Optional<TombstoneFile> parse(ParcelFileDescriptor pfd, String[] outProcessName) {
             final FileInputStream is = new FileInputStream(pfd.getFileDescriptor());
             final ProtoInputStream stream = new ProtoInputStream(is);
 
@@ -463,6 +463,7 @@ public final class NativeTombstoneManager {
                         case (int) Tombstone.COMMAND_LINE:
                             if (processName == null) {
                                 processName = stream.readString(Tombstone.COMMAND_LINE);
+                                outProcessName[0] = processName;
                             }
                             break;
 
