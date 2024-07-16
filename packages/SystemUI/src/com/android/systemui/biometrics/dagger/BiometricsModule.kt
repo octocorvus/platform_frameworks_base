2c47c429269724b3bf3098e432bca1546778785a
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-07-16 17:28:12 +0300

    fix touch-to-unlock on Google devices with side fingerprint scanner
    Before this change, touch-to-unlock was always active there, regardless of the setting value.

--- packages/SystemUI/src/com/android/systemui/biometrics/dagger/BiometricsModule.kt
+++ packages/SystemUI/src/com/android/systemui/biometrics/dagger/BiometricsModule.kt
@@ -25,6 +25,8 @@ import com.android.launcher3.icons.IconProvider
 import com.android.systemui.CoreStartable
 import com.android.systemui.biometrics.AuthController
 import com.android.systemui.biometrics.EllipseOverlapDetectorParams
+import com.android.systemui.biometrics.FingerprintInteractiveToAuthProvider
+import com.android.systemui.biometrics.FingerprintInteractiveToAuthGoogleProviderImpl
 import com.android.systemui.biometrics.UdfpsUtils
 import com.android.systemui.biometrics.data.repository.BiometricStatusRepository
 import com.android.systemui.biometrics.data.repository.BiometricStatusRepositoryImpl
@@ -106,6 +108,10 @@ interface BiometricsModule {
     @BindsOptionalOf fun authContextPlugins(): AuthContextPlugins
 
     @BindsOptionalOf fun deviceEntryUnlockTrackerViewBinder(): DeviceEntryUnlockTrackerViewBinder
+
+    @Binds
+    @SysUISingleton
+    fun fingerprintInteractiveToAuthProvider(impl: FingerprintInteractiveToAuthGoogleProviderImpl): FingerprintInteractiveToAuthProvider
 
     companion object {
         @Provides
