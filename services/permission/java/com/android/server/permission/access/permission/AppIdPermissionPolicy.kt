940cc46604eaf0d1064429a4e1e0ac6029c38a21
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-08-10 17:58:09 +0300

    new PermissionService: implement updatePermissions()

--- services/permission/java/com/android/server/permission/access/permission/AppIdPermissionPolicy.kt
+++ services/permission/java/com/android/server/permission/access/permission/AppIdPermissionPolicy.kt
@@ -938,7 +938,7 @@ class AppIdPermissionPolicy : SchemePolicy() {
         }
     }
 
-    private fun MutateStateScope.evaluateAllPermissionStatesForPackageAndUser(
+    fun MutateStateScope.evaluateAllPermissionStatesForPackageAndUser(
         packageState: PackageState,
         userId: Int,
         installedPackageState: PackageState?,
