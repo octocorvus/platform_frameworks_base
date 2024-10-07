c8458233576eddc6bc38fce1af0d66017945f59c
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-10-07 18:59:24 +0300

    include path of ANR stack traces file in ApplicationErrorReport
    ANR stack traces file contains stack traces of all app's threads and of all threads of relevant or
    possibly relevant system processes, such as system_server.

--- core/java/android/app/ActivityManager.java
+++ core/java/android/app/ActivityManager.java
@@ -3857,6 +3857,10 @@ public class ActivityManager {
          */
         public byte[] crashData = null;
 
+        /** @hide */
+        @Nullable
+        public String tracesFilePath;
+
         public ProcessErrorStateInfo() {
         }
 
@@ -3875,6 +3879,7 @@ public class ActivityManager {
             dest.writeString(shortMsg);
             dest.writeString(longMsg);
             dest.writeString(stackTrace);
+            dest.writeString(tracesFilePath);
         }
 
         public void readFromParcel(Parcel source) {
@@ -3886,6 +3891,7 @@ public class ActivityManager {
             shortMsg = source.readString();
             longMsg = source.readString();
             stackTrace = source.readString();
+            tracesFilePath = source.readString();
         }
 
         public static final @android.annotation.NonNull Creator<ProcessErrorStateInfo> CREATOR =
