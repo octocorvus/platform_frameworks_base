216bb6417a8923c1a645822ac5bf8d12f4f574a9
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2025-11-12 16:07:24 +0200

    backend part of 2FA changes

--- packages/SystemUI/src/com/android/systemui/util/wrapper/LockPatternCheckerWrapper.kt
+++ packages/SystemUI/src/com/android/systemui/util/wrapper/LockPatternCheckerWrapper.kt
@@ -17,6 +17,7 @@
 package com.android.systemui.util.wrapper
 
 import android.os.AsyncTask
+import com.android.internal.widget.LockDomain
 import com.android.internal.widget.LockPatternChecker
 import com.android.internal.widget.LockPatternChecker.OnCheckCallback
 import com.android.internal.widget.LockPatternChecker.OnVerifyCallback
@@ -43,7 +44,17 @@ class LockPatternCheckerWrapper @Inject constructor() {
         userId: Int,
         callback: OnCheckCallback,
     ): AsyncTask<*, *, *> {
-        return LockPatternChecker.checkCredential(utils, credential, userId, callback)
+        return checkCredential(utils, LockDomain.Primary, credential, userId, callback)
+    }
+
+    fun checkCredential(
+        utils: LockPatternUtils,
+        lockDomain: LockDomain,
+        credential: LockscreenCredential,
+        userId: Int,
+        callback: OnCheckCallback,
+    ): AsyncTask<*, *, *> {
+        return LockPatternChecker.checkCredential(utils, lockDomain, credential, userId, callback)
     }
 
     /**
