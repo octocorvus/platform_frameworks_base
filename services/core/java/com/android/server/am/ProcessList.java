GrapheneOS-Archive/platform_frameworks_base-old

add system API for getting app info by PID

services/core/java/com/android/server/am/ProcessList.java
@@ -64,6 +64,7 @@ import android.annotation.NonNull;
 import android.annotation.Nullable;
 import android.app.ActivityManager;
 import android.app.ActivityManager.ProcessCapability;
+import android.app.ActivityManagerInternal.ProcessRecordSnapshot;
 import android.app.ActivityThread;
 import android.app.AppGlobals;
 import android.app.AppProtoEnums;
@@ -119,6 +120,7 @@ import android.util.ArraySet;
 import android.util.DebugUtils;
 import android.util.EventLog;
 import android.util.LongSparseArray;
+import android.util.LruCache;
 import android.util.Pair;
 import android.util.Slog;
 import android.util.SparseArray;
@@ -2814,11 +2816,15 @@ public final class ProcessList {
         return true;
     }
 
+    private final LruCache<Integer, ProcessRecord> mRecentlyRemovedProcesses = new LruCache<>(50);
+
     @GuardedBy("mService")
     void removeLruProcessLocked(ProcessRecord app) {
         int lrui = mLruProcesses.lastIndexOf(app);
         if (lrui >= 0) {
             synchronized (mProcLock) {
+                mRecentlyRemovedProcesses.put(app.mPid, app);
+
                 if (!app.isKilled()) {
                     if (app.isPersistent()) {
                         Slog.w(TAG, "Removing persistent process that hasn't been killed: " + app);
@@ -2844,6 +2850,36 @@ public final class ProcessList {
             }
         }
         mService.removeOomAdjTargetLocked(app, true);
+    }
+
+    private static ProcessRecordSnapshot snapshotProcessRecord(ProcessRecord pr) {
+        return new ProcessRecordSnapshot(pr.mPid, pr.uid, pr.userId, pr.processName, pr.info,
+            pr.getPkgList().getPackageList());
+    }
+
+    @Nullable
+    public ProcessRecordSnapshot getProcessRecordByPid(int pid) {
+        ProcessRecord pr;
+
+        var pidMap = mService.mPidsSelfLocked;
+        synchronized (pidMap) {
+            pr = pidMap.get(pid);
+        }
+
+        if (pr != null) {
+            return snapshotProcessRecord(pr);
+        }
+
+        synchronized (mService) {
+            synchronized (mProcLock) {
+                ProcessRecord rrp = mRecentlyRemovedProcesses.get(Integer.valueOf(pid));
+                if (rrp != null) {
+                    return snapshotProcessRecord(rrp);
+                }
+            }
+        }
+
+        return null;
     }
 
     @GuardedBy({"mService", "mProcLock"})

octocorvus/platform_frameworks_base

services/core/java/com/android/server/am/ProcessList.java
@@ -72,6 +72,7 @@ import android.annotation.SpecialUsers.CanBeALL;
 import android.annotation.UserIdInt;
 import android.app.ActivityManager;
 import android.app.ActivityManager.ProcessCapability;
+import android.app.ActivityManagerInternal.ProcessRecordSnapshot;
 import android.app.ActivityThread;
 import android.app.AppGlobals;
 import android.app.AppProtoEnums;
@@ -128,6 +129,7 @@ import android.util.ArraySet;
 import android.util.DebugUtils;
 import android.util.EventLog;
 import android.util.LongSparseArray;
+import android.util.LruCache;
 import android.util.Pair;
 import android.util.Slog;
 import android.util.SparseArray;
@@ -3001,12 +3003,16 @@ public final class ProcessList implements ProcessStateController.ProcessLruUpdat
         return true;
     }
 
+    private final LruCache<Integer, ProcessRecord> mRecentlyRemovedProcesses = new LruCache<>(50);
+
     @GuardedBy("mService")
     @Override
     public void removeLruProcessLocked(ProcessRecord app) {
         int lrui = mLruProcesses.lastIndexOf(app);
         if (lrui >= 0) {
             synchronized (mProcLock) {
+                mRecentlyRemovedProcesses.put(app.mPid, app);
+
                 if (!app.isKilled()) {
                     if (app.isPersistent()) {
                         Slog.w(TAG, "Removing persistent process that hasn't been killed: " + app);
@@ -3033,6 +3039,36 @@ public final class ProcessList implements ProcessStateController.ProcessLruUpdat
             }
         }
         mService.removeOomAdjTargetLocked(app, true);
+    }
+
+    private static ProcessRecordSnapshot snapshotProcessRecord(ProcessRecord pr) {
+        return new ProcessRecordSnapshot(pr.mPid, pr.uid, pr.userId, pr.processName, pr.info,
+            pr.getPkgList().getPackageNames());
+    }
+
+    @Nullable
+    public ProcessRecordSnapshot getProcessRecordByPid(int pid) {
+        ProcessRecord pr;
+
+        var pidMap = mService.mPidsSelfLocked;
+        synchronized (pidMap) {
+            pr = pidMap.get(pid);
+        }
+
+        if (pr != null) {
+            return snapshotProcessRecord(pr);
+        }
+
+        synchronized (mService) {
+            synchronized (mProcLock) {
+                ProcessRecord rrp = mRecentlyRemovedProcesses.get(Integer.valueOf(pid));
+                if (rrp != null) {
+                    return snapshotProcessRecord(rrp);
+                }
+            }
+        }
+
+        return null;
     }
 
     @GuardedBy({"mService", "mProcLock"})